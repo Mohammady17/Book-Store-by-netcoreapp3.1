bb26e98fcc612600d22f04003a69f35a6506f790
Mohammad <mohammad.yi2020@gmail.com>
2026-08-16 22:00:47 +0330

    Model validation added on DTO's

--- Models/UpdateBookDto.cs
+++ Models/UpdateBookDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Book_api_core.Validations;
+
 namespace Book_api_core.Models
 {
     public class UpdateBookDto
     {
+        [Required(ErrorMessage = "لطفا عنوان کتاب را وارد کنید")]
+        [BanKeyword]
         public string Title { get; set; }
+
+        [MaxLength(40, ErrorMessage = "طول توضیحات نمیتواند بیشتر از 40 کاراکتر باشد")]
         public string Description { get; set; }
+
+        [Range(1000.0, 1000000.0, ErrorMessage = "مبلغ باید بین 1000 تا 1 میلیون تومان باشد")]
         public int Amount { get; set; }
     }
 }
---
+++ Validations/BanKeywordAttribute.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Book_api_core.Validations
+{
+    public class BanKeywordAttribute : ValidationAttribute
+    {
+        public BanKeywordAttribute()
+        {
+            BanKeywords = new List<string>()
+            {
+                "shit", "fuck"
+            };
+        }
+        public List<string> BanKeywords { get; set; }
+        public override string FormatErrorMessage(string name)
+        {
+            return "لطفا از کلمات ممنوعه در عنوان استفاده نکنید";
+        }
+
+        public override bool IsValid(object value)
+        {
+            var title = (string) value;
+            if (BanKeywords.Contains(title.ToLower())) return false;
+            return true;
+        }
+    }
+}
